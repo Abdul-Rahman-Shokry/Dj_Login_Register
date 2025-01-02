9ed1f974a88029fb99b6f3f8105fab1ca3645f6b
Abdul-Rahman-Shokry <abdul.rahman.shokry2004@gmail.com>
2025-01-02 18:33:48 +0200

    authView

---
+++ base/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+
+def authView(request):
+    form = UserCreationForm()
+    return render(request, "registration/signup.html", {"forms": form})
